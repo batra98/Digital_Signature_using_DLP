369e0d29985622adbf15d53808b3edfc18c6a36a
batra98 <batragaurav2616@gmail.com>
2020-04-02 19:42:21 +0530

    added n bit support

--- digital_sig.py
+++ digital_sig.py
@@ -1,16 +1,18 @@
 from random import SystemRandom
+from Crypto.Util import number
+import gensafeprime
 
 rand = SystemRandom()
 
 class Alice():
 	def __init__(self,g,p,n):
 		self.private_key = rand.getrandbits(n) ## x
-		self.public_key = pow(g,self.private_key)%p ## g^x % p
+		self.public_key = pow(g,self.private_key,p) ## g^x % p
 		self.g = g
 
 	def send_random(self,p,n):
 		self.r = rand.getrandbits(n) ## r
-		return pow(g,self.r)%p ## g ^ r % p
+		return pow(g,self.r,p) ## g ^ r % p
 
 	def receive_c(self,p,n,c):
 		self.z = c*self.private_key + self.r ## cx+r
@@ -28,8 +30,8 @@ class Bob():
 		return rand.getrandbits(n)%p
 
 	def verify(self,g,z,c,p):
-		X = pow(g,z)%p
-		Y = ((pow(self.public_key,c)%p)*(self.m1%p))%p
+		X = pow(g,z,p)
+		Y = ((pow(self.public_key,c,p))*(self.m1%p))%p
 
 		if(X == Y):
 			print("Yes")
@@ -38,9 +40,20 @@ class Bob():
 
 
 
-p = 17
-g = 3
-n = 5
+# p = 17
+# g = 3
+# n = 5
+
+n = 1024
+p = gensafeprime.generate(n)
+q = (p-1)//2
+
+t = 1
+while t == 1:
+	h = number.getRandomRange(2,p-2)
+	t = pow(h,2,p)
+
+g = h
 
 M = rand.getrandbits(n)
 
@@ -48,6 +61,7 @@ A = Alice(g,p,n)
 B = Bob(g,A.public_key,p)
 
 print(A.private_key)
+print(A.public_key)
 
 m1 = A.send_random(p,n)
 B.receive_random(m1)
